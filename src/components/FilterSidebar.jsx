e358f02b78f77f9b25e85e865469dc54fcd5d22e
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-04-02 13:11:01 +0500

    text

--- src/components/FilterSidebar.jsx
+++ src/components/FilterSidebar.jsx
@@ -7,16 +7,16 @@ const FilterSidebar = ({onSortByChange, onFilterByRating, onClearFilters}) => {
       <nav className="flex flex-col items-center justify-center">
         <h2 className="text-2xl font-bold mb-2">Filter Out</h2>
         <ul className="flex flex-col md:flex-row gap-2 text-xl font-medium text-center">
-            <li className="border p-2 bg-gray-300 rounded-md">
+            <li className="border p-2 bg-gray-100 rounded-md">
                 <button onClick={()=> onSortByChange("lowest")}>Low Price</button>    
             </li>
-            <li className="border p-2 bg-gray-300 rounded-md ">
+            <li className="border p-2 bg-gray-100 rounded-md ">
                 <button onClick={()=> onSortByChange("highest")}>High Price</button>
             </li>
-            <li className="border p-2 bg-gray-300 rounded-md ">
+            <li className="border p-2 bg-gray-100 rounded-md ">
                 <button onClick={()=> onFilterByRating(4)}>Rating</button>
             </li>
-            <li className="border p-2 bg-gray-300 rounded-md">
+            <li className="border p-2 bg-gray-100 rounded-md">
             <button onClick={onClearFilters}>Clear Filters</button>
           </li>
         </ul>
